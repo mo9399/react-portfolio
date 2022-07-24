0d3e5673dd96e286a1984ab87b65a55dd6411a07
Mo <mohamed.abdullahi@yahoo.com>
2022-07-24 00:30:21 -0700

    add header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Nav from '../Nav';
+
+function Header(props) {
+    const { currentPage, setCurrentPage } = props;
+
+    return (
+        <header>
+             <h1 href='/'>Mohamed Abdullahi</h1>
+            <div>
+                <Nav currentPage={currentPage} setCurrentPage={setCurrentPage}></Nav>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
